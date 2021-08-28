550477e1de76464f10112069118b800326b849f6
Ariel Weingarten <asweingarten@gmail.com>
2021-08-28 13:35:05 -0700

    Init commit

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,27 @@
+<div>
+	<nav>
+		<a href="/"><h3 class="home">HOME</h3></a>
+		<a href="/about"><h3 class="about">about</h3></a>
+	</nav>
+</div>
+
+<style lang="scss">
+	nav {
+		display: flex;
+		justify-content: space-between;
+		align-items: baseline;
+	}
+	a {
+		text-decoration: none;
+		color: inherit;
+	}
+	h3 {
+		margin-bottom: 0px;
+		&.home {
+			color: #444;
+		}
+		&.about {
+			color: #ff3e00;
+		}
+	}
+</style>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import '../app.scss';
+	import Nav from '$lib/Nav.svelte';
+</script>
+
+<div class="g-app-wrapper">
+	<Nav />
+	<slot />
+</div>
+
+<style lang="scss">
+	.g-app-wrapper {
+		max-width: 800px;
+		margin: 0 auto;
+		padding: 10px;
+	}
+</style>
